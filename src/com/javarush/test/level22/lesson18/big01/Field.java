72f8e7ecd454f631ce5a86c725abc764305e0beb
Peter Arnaut <arnaut.peter@gmail.com>
2016-11-22 01:09:33 +0200

    some tasks

--- src/com/javarush/test/level22/lesson18/big01/Field.java
+++ src/com/javarush/test/level22/lesson18/big01/Field.java
@@ -1,4 +1,52 @@
 package com.javarush.test.level22.lesson18.big01;
 
 public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void print() {
+
+    }
+
+    public void removeFullLines() {
+
+    }
+
+    public Integer getValue(int x, int y) {
+        return 0;
+    }
+
+    void setValue(int x, int y, int value) {
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
 }
