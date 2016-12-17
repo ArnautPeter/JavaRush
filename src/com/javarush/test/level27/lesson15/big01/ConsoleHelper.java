28d04e3a5c2ed0517a43002ec1a06328a992d9e2
Peter Arnaut <arnaut.peter@gmail.com>
2016-12-17 20:33:19 +0200

    Big22

---
+++ src/com/javarush/test/level27/lesson15/big01/ConsoleHelper.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level27.lesson15.big01;
+
+import com.javarush.test.level27.lesson15.big01.kitchen.Dish;
+
+import java.io.BufferedReader;
+import java.util.List;
+
+public class ConsoleHelper {
+
+
+}
